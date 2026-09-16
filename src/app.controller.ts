69a31fcf3204eaa7d9662bf057b3f5e41ec6c9c0
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2026-09-16 12:17:26 +0200

    I am trying I swear - the commit

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service.js';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  getHello() {
+    return {
+      bemutatkozas: 'Phan Thi Linh Tra, 13.E osztályos tanuló.'
+    }
+  }
+
+  @Get('bekezdesek')
+  @Render('bekezdesek')
+  getSzoveg() {
+    return {
+      
+    }
+  }
+}
